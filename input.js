b21fb728e9d9065157a981b5874b0522710a0908
DanielYuanxk <dyxk1234@hotmail.com>
2023-01-18 16:31:13 -0500

    snake finished

--- input.js
+++ input.js
@@ -1,4 +1,6 @@
-const setupInput = function () {
+let connection;
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,6 +12,18 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === "\u0003") {
     process.exit();
+  } // your code here
+  if (key === "\u0077") {
+    connection.write("Move: up");
+  } // your code here
+  if (key === "\u0073") {
+    connection.write("Move: down");
+  } // your code here
+  if (key === "\u0061") {
+    connection.write("Move: left");
+  } // your code here
+  if (key === "\u0064") {
+    connection.write("Move: right");
   } // your code here
 };
 module.exports = { setupInput };
